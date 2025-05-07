ab0c4ce11f9748ea09fe9bcaeeadeee2e0f53b6f
axryap27 <aarya27@gmail.com>
2025-05-07 16:59:42 -0500

    Initial commit from remote ssh - step function and other helpers

--- debugger.cpp
+++ debugger.cpp
@@ -3,12 +3,12 @@
 //
 // Debugger for nuPython, C++ edition! Provides a simple gdb-like
 // interface, with support for multiple breakpoints and step-by-step
-// execution of nuPython, including if stmts. Uses nuPython interpreter 
+// execution of nuPython, including if stmts. Uses nuPython interpreter
 // as execution engine.
 //
 // Author:
-//   << YOUR NAME>
-// 
+//   Aarya Patel
+//
 // Initial template:
 //   Ata Guvendi
 //   Prof. Hummel
@@ -20,17 +20,26 @@
 #include <string>
 
 #include "debugger.h"
+#include "programgraph.h"
+#include "scanner.h"
+#include "execute.h"
 
 using namespace std;
-
 
 //
 // constructor:
 //
-Debugger::Debugger(struct STMT* program)
+Debugger::Debugger(struct STMT *program)
 {
+  this->program = program;                   // save the root for "pg", etc.
+  this->curr_stmt = program;                // start at the beginning
+  if (program != nullptr){
+    this->next_stmt = get_next_stmt(program);
+  }
+  else{
+    this->next_stmt = nullptr;
+  }
 }
-
 
 //
 // destructor:
@@ -38,7 +47,6 @@ Debugger::Debugger(struct STMT* program)
 Debugger::~Debugger()
 {
 }
-
 
 //
 // run:
@@ -48,43 +56,117 @@ Debugger::~Debugger()
 void Debugger::run()
 {
   string cmd;
-  
+
   //
   // command loop until quit is entered:
   //
-  while (true) {
-    
+  while (true)
+  {
+
     cout << endl;
     cout << "Enter a command, type h for help. Type r to run. > " << endl;
     cin >> cmd;
-    
-    if (cmd == "q") {
+
+    if (cmd == "q")
+    {
 
       break;
     }
-    else if (cmd == "h") {
-      
+    else if (cmd == "h")
+    {
+
       cout << "Available commands:"
-        << endl << "r -> Run the program / continue from a breakpoint"
-        << endl << "s -> Step to next stmt by executing current stmt"
-        << endl << "b n -> Breakpoint at line n"
-        << endl << "rb n -> Remove breakpoint at line n"
-        << endl << "lb -> List all breakpoints"
-        << endl << "cb -> Clear all breakpoints"
-        << endl << "p varname -> Print variable"
-        << endl << "pg -> Program graph"
-        << endl << "sm -> Show memory contents"
-        << endl << "ss -> Show state of debugger"
-        << endl << "w -> What line are we on?"
-        << endl << "q -> Quit the debugger"
-        << endl;
+           << endl
+           << "r -> Run the program / continue from a breakpoint"
+           << endl
+           << "s -> Step to next stmt by executing current stmt"
+           << endl
+           << "b n -> Breakpoint at line n"
+           << endl
+           << "rb n -> Remove breakpoint at line n"
+           << endl
+           << "lb -> List all breakpoints"
+           << endl
+           << "cb -> Clear all breakpoints"
+           << endl
+           << "p varname -> Print variable"
+           << endl
+           << "pg -> Program graph"
+           << endl
+           << "sm -> Show memory contents"
+           << endl
+           << "ss -> Show state of debugger"
+           << endl
+           << "w -> What line are we on?"
+           << endl
+           << "q -> Quit the debugger"
+           << endl;
     }
-    else {
-      
+    else if (cmd == "s")
+    {
+
+      // line #
+      // "#: stmt"
+      // some tracking to make sure we are going to get stmt/line --> pointer to stmt graph prob
+    }
+    else if (cmd == "w"){
+      Debugger::step();
+    }
+    else if (cmd == "pg"){
+      programgraph_print(program);
+    }
+    else
+    {
+
       cout << "unknown command" << endl;
     }
-    
-  }//while
-  
-}//run
 
+  } // while
+
+} // run
+
+//
+// Get_Next_Stmt --- See header file for comments
+struct STMT* Debugger::get_next_stmt(struct STMT* stmt)
+{
+    if (stmt == nullptr){
+      return nullptr;
+    }
+    else if (stmt->stmt_type == STMT_ASSIGNMENT){
+      return stmt->types.assignment->next_stmt;
+    }
+    else if (stmt->stmt_type == STMT_FUNCTION_CALL){
+      return stmt->types.function_call->next_stmt;
+    }
+    else if (stmt->stmt_type == STMT_IF_THEN_ELSE){
+      return stmt->types.if_then_else->next_stmt;
+    }
+    else if (stmt->stmt_type == STMT_PASS){
+      return stmt->types.pass->next_stmt;
+    }
+    else if (stmt->stmt_type == STMT_WHILE_LOOP){
+      return stmt->types.while_loop->next_stmt;
+    }
+    else 
+      return nullptr;
+} // get_next_stmt
+
+
+// step function
+// see header file for comments
+void Debugger::step(){
+  if(curr_stmt == nullptr){
+    cout << "program has completed" << endl;
+    return;
+  }
+  // temp variable for next_stmt 
+  struct STMT* saved_next = next_stmt;
+  set_next_stmt(curr_stmt, nullptr);
+
+  execute(curr_stmt, nullptr);
+
+  // relink and move to next line
+  set_next_stmt(curr_stmt, saved_next);
+  curr_stmt = saved_next;
+  next_stmt = get_next_stmt(curr_stmt);
+}
--- debugger.h
+++ debugger.h
@@ -7,7 +7,7 @@
 // as execution engine.
 //
 // Author:
-//   << YOUR NAME>
+//   Aarya Patel
 // 
 // Initial template:
 //   Ata Guvendi
@@ -26,8 +26,11 @@ using namespace std;
 class Debugger
 {
 private:
-
-
+  struct STMT* program; //root of the program graph struct
+  struct STMT* curr_stmt; //statement we are on 
+  struct STMT* next_stmt; // next statement
+  struct STMT* mem; // execution needed memory
+  string state; 
 
 public:
   Debugger(struct STMT* program);
@@ -35,5 +38,32 @@ public:
   ~Debugger();
 
   void run();
+
+  // Get_Next_Stmt
+  //
+  // Gets the next stmt based on the statement type of the current statement inputted
+  // Gets the pointer pointing to the specific stmt type and then gets the pointer 
+  // to the next stmt in the program graph
+  struct STMT* get_next_stmt(struct STMT* stmt);
+
+
+  //
+  // set_next_stmt
+  //
+  // Sets the `next_stmt` pointer of the given nuPython statement to a new
+  // value. This is used to temporarily break or restore links in the program
+  // graph so that only one statement is executed at a time (e.g., during step()).
+  //
+  void set_next_stmt(struct STMT* stmt, struct STMT* next);
+
+  // step
+  //
+  // Performs a "step" operation on the program by linking and unlinking
+  // the next_stmt and curr_stmt pointers along the program graph, while also executing the lines 
+  // that correspond to the curr_stmt as it traverses the program graph
+  void step();
+
+  
+
 };
 
